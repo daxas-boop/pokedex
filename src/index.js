34a96a38b02854ab4dc1cfa7babdc7d8f0fbb10d
matias <matias.daniel.zabalegui@gmail.com>
2023-02-16 20:44:12 -0300

    Permite mirar los detalles de un pokemon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import { traerPokemones } from './api.js';
-import { mostrarCargandoPagina, esconderCargandoPagina, mostrarPokemones } from './ui.js';
+import { traerPokemones, traerPokemon } from './api.js';
+import { mostrarCargandoPagina, esconderCargandoPagina, mostrarPokemones, mostrarDatosPokemon } from './ui.js';
 import { actualizarPaginacion } from './paginacion.js';
 
+function esCartaSinVoltear($elemento) {
+  return $elemento.classList.contains('carta') && !$elemento.classList.contains('carta-volteada');
+}
+
+function manejarClickCartaPokemon() {
+  document.querySelector('#cartas').onclick = async (e) => {
+    if (esCartaSinVoltear(e.target)) {
+      const $carta = e.target;
+      $carta.classList.add('carta-volteada', 'carta-cargando');
+      const nombrePokemon = $carta.dataset.nombre;
+      traerPokemon(nombrePokemon).then((pokemon) => {
+        mostrarDatosPokemon($carta, pokemon);
+        $carta.classList.remove('carta-cargando');
+      });
+    }
+  };
+}
+
 function cambiarPagina(numeroPagina = 1) {
-  const LIMITE = 36;
+  const LIMITE = 24;
   const offset = (numeroPagina - 1) * LIMITE;
   mostrarCargandoPagina();
   document.querySelector('#cartas').innerHTML = '';
@@ -11,6 +29,7 @@ function cambiarPagina(numeroPagina = 1) {
     const cantidadPaginas = Math.ceil(pokemones.cantidad / LIMITE);
     actualizarPaginacion(cantidadPaginas, numeroPagina, cambiarPagina);
     mostrarPokemones(pokemones.resultados);
+    manejarClickCartaPokemon();
     esconderCargandoPagina();
   });
 }
--- src/ui.js
+++ src/ui.js
@@ -20,6 +20,44 @@ function crearTituloPokemon(nombrePokemon, numeroPokemon) {
   return $titulo;
 }
 
+function crearDatosPokemon(pokemon) {
+  const $datos = document.createElement('div');
+  const $tipo = document.createElement('p');
+  const $peso = document.createElement('p');
+  const $altura = document.createElement('p');
+  const $exp = document.createElement('p');
+  $peso.className = 'badge m-0 p-1';
+  $tipo.className = 'badge m-0 p-1';
+  $exp.className = 'badge m-0 p-1';
+  $altura.className = 'badge m-0 p-1';
+  $datos.className = 'd-flex flex-column align-items-start ms-4 mt-4 datos-carta';
+  $tipo.innerText = 'Type: ' + pokemon.tipo[0].toUpperCase() + pokemon.tipo.slice(1);
+  $peso.innerText = 'Weight: ' + pokemon.peso / 10 + 'Kg';
+  $altura.innerText = 'Height: ' + pokemon.altura / 10 + 'm';
+  $exp.innerText = 'Base Exp.: ' + pokemon.experienciaBase;
+  $datos.appendChild($tipo);
+  $datos.appendChild($peso);
+  $datos.appendChild($altura);
+  $datos.appendChild($exp);
+  return $datos;
+}
+
+function crearImagenPokemon(src) {
+  const $imagen = document.createElement('img');
+  $imagen.className = 'card-img imagen-carta';
+  $imagen.src = '';
+  $imagen.classList.add('imagen-cargando');
+  $imagen.onload = function () {
+    $imagen.classList.remove('imagen-cargando');
+  };
+  if (src) {
+    $imagen.src = src;
+  } else {
+    $imagen.src = './assets/imagenes/nodisponible.png';
+  }
+  return $imagen;
+}
+
 function crearCartaPokemon(nombrePokemon, numeroPokemon) {
   const $carta = document.createElement('div');
   $carta.className = 'card carta';
@@ -36,4 +74,15 @@ function mostrarPokemones(pokemones) {
   });
 }
 
-export { esconderCargandoPagina, mostrarCargandoPagina, mostrarPokemones };
+function mostrarDatosPokemon($carta, pokemon) {
+  $carta.innerHTML = '';
+  const $titulo = crearTituloPokemon(pokemon.nombre, pokemon.numero);
+  const $datos = crearDatosPokemon(pokemon);
+  const $imagen = crearImagenPokemon(pokemon.imagen);
+  $carta.classList.add(pokemon.tipo);
+  $carta.appendChild($titulo);
+  $carta.appendChild($datos);
+  $carta.appendChild($imagen);
+}
+
+export { esconderCargandoPagina, mostrarCargandoPagina, mostrarPokemones, mostrarDatosPokemon };
